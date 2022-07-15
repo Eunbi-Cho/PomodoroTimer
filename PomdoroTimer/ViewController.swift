98cecdd0f61752738ba3aaa2d92403dd9666feda
Eunbi Cho <moomb70@gmail.com>
2022-07-15 23:26:14 +0900

    feat[#2]: 주석 추가

--- PomdoroTimer/ViewController.swift
+++ PomdoroTimer/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// 타이머 상태
 enum TimerStatus {
     case start
     case pause
@@ -32,23 +33,29 @@ class ViewController: UIViewController {
         self.configureToggleButton()
     }
     
+    // 버튼이 눌렸을 때 가려지고 나타나는 기능
     func setTimerInfoViewVisable(isHidden: Bool) {
         self.timerLabel.isHidden = isHidden
         self.progressView.isHidden = isHidden
     }
     
+    // 버튼이 눌렸을 때 버튼 타이틀 바뀌는 기능
     func configureToggleButton() {
         self.toggleButton.setTitle("Start", for: .normal)
         self.toggleButton.setTitle("Pause", for: .selected)
     }
     
+    // 취소 버튼 기능
     @IBAction func tabCancelButton(_ sender: UIButton) {
         switch self.timerStatus {
         case .start, .pause:
+            // 시작, 정지 상태일 때 버튼이 활성화되고, 정지 상태로 변함
             self.timerStatus = .end
             self.cancelButton.isEnabled = false
+            // 타이머뷰 가려지고 피커 나타남
             self.setTimerInfoViewVisable(isHidden: true)
             self.datePicker.isHidden = false
+            // 무조건 시작 버튼으로 변함
             self.toggleButton.isSelected = false
             
         default:
@@ -56,24 +63,30 @@ class ViewController: UIViewController {
         }
     }
     
+    // 토글 버튼 기능
     @IBAction func tabToggleButton(_ sender: UIButton) {
         self.duration = Int(self.datePicker.countDownDuration)
         switch self.timerStatus {
+            
+        // 시작 상태일 때 멈춤 상태로 바꾸기
         case .start:
             self.timerStatus = .pause
             self.toggleButton.isSelected = false
-            
+        
         case .end:
+            // 종료 상태일 때 시작 상태로 바꾸기
             self.timerStatus = .start
+            self.toggleButton.isSelected = true
+            // 피커 사라지고 타이머 나타남
             self.setTimerInfoViewVisable(isHidden: false)
             self.datePicker.isHidden = true
-            self.toggleButton.isSelected = true
+            // 취소 버튼 활성화됨
             self.cancelButton.isEnabled = true
             
         case .pause:
+            // 멈춤 상태일 때 시작 상태로 바꾸기
             self.timerStatus = .start
             self.toggleButton.isSelected = true
-            
         }
     }
 
